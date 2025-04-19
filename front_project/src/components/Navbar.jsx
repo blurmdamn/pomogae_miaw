e022845a2d299f1fd07221e373f3d86b407ca54c
Diana <lidiana.0455@gmail.com>
2025-04-19 18:24:42 +0500

    Уведомления шторка

--- front_project/src/components/Navbar.jsx
+++ front_project/src/components/Navbar.jsx
@@ -6,6 +6,8 @@ import {
   UserPlusIcon,
 } from "@heroicons/react/24/outline";
 import { CubeTransparentIcon } from "@heroicons/react/24/solid";
+import NotificationsDropdown from "./NotificationsDropdown";
+
 const Navbar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
@@ -46,13 +48,16 @@ const Navbar = () => {
 
         <div className="flex items-center space-x-6 text-sm font-medium">
           {isLoggedIn ? (
-            <button
-              onClick={handleLogout}
-              className="flex items-center gap-2 px-3 py-1 border border-red-400 rounded hover:bg-red-500 hover:text-white transition"
-            >
-              <ArrowLeftOnRectangleIcon className="w-5 h-5" />
-              Выйти
-            </button>
+            <>
+              <NotificationsDropdown />
+              <button
+                onClick={handleLogout}
+                className="flex items-center gap-2 px-3 py-1 border border-red-400 rounded hover:bg-red-500 hover:text-white transition"
+              >
+                <ArrowLeftOnRectangleIcon className="w-5 h-5" />
+                Выйти
+              </button>
+            </>
           ) : (
             <>
               <Link
@@ -70,7 +75,6 @@ const Navbar = () => {
                 <UserPlusIcon className="w-5 h-5" />
                 Регистрация
               </Link>
-
             </>
           )}
         </div>
